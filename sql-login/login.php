28785c8a4f44a36896f1cf7910d6f7bf5f8e5f03
Andres <macias.andres02@gmail.com>
2017-11-14 16:06:34 -0600

    Add files via upload

---
+++ sql-login/login.php
@@ -0,0 +1,148 @@
+<?php
+//pevents chrome error when using the back button to return to this page
+session_cache_limiter('none');
+session_start();
+
+
+$message = "";
+
+$_SESSION['validUser'] = "no";
+
+//checks if user is valid
+
+if ($_SESSION['validUser'] == "yes") {
+    // if already signed in.
+    $message = "Welcome back!";
+  
+} else {
+    //checks if page was called fom a submitted form
+    
+    if (isset($_POST['submitLogin'])) {
+      
+        //pull username and password from form
+        $inUsername = $_POST['loginUsername'];
+        $inPassword = $_POST['loginPassword'];
+//connect to database
+        include 'connectPDO.php';
+
+$query = "SELECT event_user_name,event_user_password FROM event_user WHERE event_user_name = :event_user_name AND event_user_password = :event_user_password";
+
+//prepare the query and bind parameters
+$stmt = $conn->prepare($query);
+$stmt->bindValue(':event_user_name',$inUsername, PDO::PARAM_STR);
+$stmt->bindValue(':event_user_password', $inPassword, PDO::PARAM_STR);
+$stmt->execute();
+
+    //If this is a valid user there should be ONE row only
+        if ($stmt->rowCount() == 1) {
+            global $username;
+            //this is a valid user so set your SESSION variable
+            $userName = $inUsername;
+            $_SESSION['validUser'] = "yes";
+            $message = "Welcome Back! $userName";
+            //Valid User can do the following things:
+        } else {
+            //error in processing login.  Logon Not Found...
+            
+            $_SESSION['validUser'] = "no";
+            $message = "Sorry, there was a problem with your username or password. Please try again.";
+        }
+        $conn = null;
+
+
+    } //end if the form was submitted
+
+
+}//end else valid user
+
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>wdv341 Events login</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<style>
+body {
+    background-color: black;
+    color:white;
+}
+
+.loginArea {
+    margin-top: 5%;
+text-align: center;
+}
+
+.loginForm {
+    border: 1px solid white;
+}
+
+
+</style>
+
+
+</head>
+<body>
+    <div class="container loginArea">
+
+   
+<section>
+<div class="row">
+    <div class="col-sm-12">
+        <h1>wdv341 intro php</h1>
+        <h2>Event admin system</h2>
+        <h2><?php echo $message?></h2>
+    </div>
+</div>
+</section>
+<section class="loginForm">
+<div class="row">
+    <div class="col-sm-12">
+            <?php
+            if($_SESSION['validUser'] == "yes") 
+            {
+            ?>
+            <h3>Event Administrator Options</h3>
+                    <p><a href="eventsForm.php">Input new events</a></p>
+                    <p><a href="selectEvents.php">List of events</a></p>
+                    <p><a href="logout.php">Logout of Admin System</a></p>	
+            <?php
+            }
+            else 
+            {
+                ?>
+                <h2>Please login to the Administrator System</h2>
+                            <form method="post" name="loginForm" action="login.php" >
+                              <p>Username: <input name="loginUsername" type="text" /></p>
+                              <p>Password: <input name="loginPassword" type="password" /></p>
+                              <p><input name="submitLogin" value="Login" type="submit" /> <input name="" type="reset" />&nbsp;</p>
+                            </form>
+            <?php
+            }
+            
+            ?>
+    </div>
+</div>
+</section>
+</div>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ sql-login/updateEvent.php
@@ -0,0 +1,251 @@
+<?php
+session_start();
+if($_SESSION['validUser'] =="yes") {
+	
+	
+	include 'connectPDO.php';		//connects to the database
+	$event_name = "";
+    $event_description = "";
+    $event_presenter = "";
+    $event_date = "";
+	$event_time = "";
+	
+    $event_name_error = "";
+    $event_description_error = "";
+    $event_presenter_error = "";
+    $event_date_error = "";
+    $event_time_error = "";
+	$message = "";
+	$validForm = false;
+
+	//get event ID
+	$event_id = filter_input(INPUT_POST, 'event_id',FILTER_VALIDATE_INT);
+	
+	//get the event that matches the event ID
+			$queryEvent = 'SELECT * FROM wdv341_event WHERE event_id = :event_id';
+			$stmt1 = $conn->prepare($queryEvent);
+			$stmt1->bindValue(':event_id', $event_id);
+			$stmt1->execute();
+			$event = $stmt1->fetch();
+			$event_name = $event['event_name'];
+			$stmt1->closeCursor();
+			
+	function validateEventName($event_name) {
+        global $validForm, $event_name_error;        //Use the GLOBAL Version of these variables instead of making them local
+     
+        if (empty($_POST["event_name"])) {
+			$event_name_error = "event name is required";
+			$validForm=false;
+	  } else {
+			$event_name_error = "";
+			//check if name only contains letters and whitespace
+		  if (!preg_match("/^[a-zA-Z0-9 ]*$/", $event_name)) {
+			  $event_name_error = "Only letters and white space allowed";
+			  $validForm = false;
+			  // cannot be all spaces
+		  } elseif (!preg_match("/^[^-\s][a-zA-Z0-9 \s-]*$/", $event_name)) {
+			  $event_name_error = "Cannot be all spaces";
+			  $validForm = false;
+		  }
+	  }
+    }//end validateName()
+    
+    function validateDescription($event_description) {
+        global $validForm, $event_description_error;
+
+        if (empty($_POST["event_description"])) {
+			$event_description_error = "event description is required";
+			$validForm=false;
+	  } else {
+			$event_description_error = "";
+			//check if name only contains letters and whitespace
+		  if (!preg_match("/^[a-zA-Z0-9 ]*$/", $event_description)) {
+			  $event_description_error = "Only letters and white space allowed";
+			  $validForm = false;
+			  // cannot be all spaces
+		  } elseif (!preg_match("/^[^-\s][a-zA-Z0-9 \s-]*$/", $event_description)) {
+			  $event_description_error = "Cannot be all spaces";
+			  $validForm = false;
+		  }
+	  }
+    } //end validateDescription()
+
+    function validatePresenter($event_presenter) {
+        global $validForm, $event_presenter_error;
+        if (empty($_POST["event_presenter"])) {
+			$event_presenter_error = "event presenter is required";
+			$validForm=false;
+	  } else {
+			$event_presenter_error = "";
+			//check if name only contains letters and whitespace
+		  if (!preg_match("/^[a-zA-Z0-9 ]*$/", $event_presenter)) {
+			  $event_presenter_error = "Only letters and white space allowed";
+			  $validForm = false;
+			  // cannot be all spaces
+		  } elseif (!preg_match("/^[^-\s][a-zA-Z0-9 \s-]*$/", $event_presenter)) {
+			  $event_presenter_error = "Cannot be all spaces";
+			  $validForm = false;
+		  }
+	  }
+    } //end validatePresenter()
+
+    function validateDate($event_date) {
+        global $validForm, $event_date_error;
+
+        if (empty($_POST["event_date"])) {
+			$event_date_error = "event date is required";
+			$validForm=false;
+	  } else {
+			$event_date_error = "";
+			
+	  }
+    } //end validateDate()
+
+    function validateTime($event_time) {
+        global $validForm, $event_time_error;
+
+       
+		if (empty($_POST["event_time"])) {
+			$event_time_error = "event time is required";
+			$validForm=false;
+	  } else {
+			$event_time_error = "";
+			//check if name only contains letters and whitespace
+	  }
+    }//end validateTime()
+	if(isset($_POST["submit"])) {
+		$event_name = $_POST['event_name'];
+		$event_description = $_POST['event_description'];
+		$event_presenter = $_POST['event_presenter'];
+		$event_date = $_POST['event_date'];
+		$event_time = $_POST['event_time'];
+		$event_id = $_POST['event_id'];	//from the hidden field of the update form
+
+
+		$validForm = true;      //switch for keeping track of any form validation errors
+		
+					validateEventName($event_name);
+					validateDescription($event_description);
+					validatePresenter($event_presenter);
+					validateDate($event_date);
+					validateTime($event_time);
+	}
+if($validForm) {
+	try {
+		$updateEvent = "UPDATE wdv341_event SET event_name ='$event_name', event_description='$event_description', event_presenter='$event_presenter', event_date='$event_date', event_time='$event_time' WHERE event_id='$event_id'";
+		
+				$stmt = $conn->prepare($updateEvent);
+				$stmt->execute();
+				
+						if ( $stmt->execute() )
+						{
+							$message = "<h1>Your record has been successfully UPDATED to the database.</h1>";
+							$message .= "<p>Please <a href='selectEvents.php'>view</a> your records.</p>";
+						}
+						else
+						{
+							$message = "<h1>You have encountered a problem.</h1>";
+							$message .= "<h2 style='color:red'>" . mysqli_error($link) . "</h2>";
+						}
+	}
+	catch(PDOException $e) {
+		$conn->rollback();
+		$message = "There has been a problem. The system administrator has been contacted. Please try again later.";
+	}
+} 
+
+} //end valid user check
+else {
+		//Invalid User attempting to access this page. Send person to Login Page
+		header('Location: login.php');
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Add Event</title>
+<style>
+.errormsg {
+		color:red;
+	}
+</style>
+	
+
+</head>
+
+<body>
+
+<h1>WDV341 Intro PHP</h1>
+<h1>Presenters Admin System Example</h1>
+<h3>UPDATE Form for Changing information on a Presenter</h3>
+<p>This page is called from the presentersSelectView.php page when you click on the Update link of a presenter. That page attaches the presenter_id to the URL of this page making it a GET parameter.</p>
+<p>This page uses that information to SELECT the requested record from the database. Then PHP is used to pull the various column values for the record and place them in the form fields as their default values. </p>
+<p>The user/customer can make changes as needed or leave the information as is. When the form is submitted and validated it will update the record in the database.</p>
+<p>Notice that this form uses a hidden field. The value of this hidden field contains the presenter_id. It is passed as one of the form name-value pairs. The submitted page will use that value to determine which record to update on the database.</p>
+
+<?php
+//If the user submitted the form the changes have been made
+if($validForm)
+{
+	echo $message;	//contains a Success or Failure output content
+
+	
+}//end if submitted
+
+else
+{	//The page needs to display the form and associated data to the user for changes
+?>
+<form id="event_Form" name="event_Form" method="post" action="updateEvent.php">
+  <p>Update the following event Information.  Place the new information in the appropriate field(s)</p>
+  <p>Event Name: 
+    <input type="text" name="event_name" id="event_name" 
+    	value="<?php echo $event['event_name']; ?>"/>	
+		<span class="errormsg"><?php echo $event_name_error; ?> </span>
+  </p>
+  <p>Event Description:  
+    <input type="text" name="event_description" id="event_description" 
+    	value="<?php echo $event['event_description']; ?>" />
+		<span class="errormsg"><?php echo $event_description_error; ?></span>
+		
+  </p>
+  <p>Event Presenter:  
+    <input type="text" name="event_presenter" id="event_presenter" 
+       	value="<?php echo $event['event_presenter']; ?>" />
+		   <span class="errormsg"><?php echo $event_presenter_error; ?></span>
+  </p>
+  <p>Event Date: 
+    <input type="text" name="event_date" id="event_date" 
+        value="<?php echo $event['event_date']; ?>" />
+		<span class="errormsg"><?php echo $event_date_error; ?></span>
+  </p>
+  <p>Event Time: 
+    <input type="text" name="event_time" id="event_time" 
+    	value="<?php echo $event['event_time']; ?>" />
+		<span class="errormsg"><?php echo $event_time_error; ?></span>
+  </p>
+  
+  	<!--The hidden form contains the record if for this record. 
+    	You can use this hidden field to pass the value of record id 
+        to the update page.  It will go as one of the name value
+        pairs from the form.
+    -->
+  	<input type="hidden" name="event_id" id="event_id"
+    	value="<?php echo $event_id ?>" />
+  
+  <p>
+    <input type="submit" name="submit" id="submit" value="Update" />
+    <input type="reset" name="button2" id="button2" value="Clear Form" />
+  </p>
+</form>
+
+<?php
+}//end else submitted
+?>
+
+</body>
+</html>
+
